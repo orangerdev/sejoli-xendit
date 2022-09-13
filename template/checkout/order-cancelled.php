0af88465bb4251c0d89c8fe942b2d671f2a3d195
asdzakky <asd.zakky@gmail.com>
2022-09-13 20:55:57 +0700

    fix[bug] xendit auto update status callbacks

--- template/checkout/order-cancelled.php
+++ template/checkout/order-cancelled.php
@@ -6,9 +6,9 @@
 ?>
 <div class="ui text container">
     <div class="thankyou">
-        <h2><?php _e('Halo ', 'sejoli-toyyibpay'); echo $order['user']->display_name; ?></h2>
+        <h2><?php _e('Halo ', 'sejoli-xendit'); echo $order['user']->display_name; ?></h2>
         <div class="thankyou-info-1">
-            <p><?php printf(__('Pesanan untuk order INV %s telah dibatalkan. Terima kasih.', 'sejoli-toyyibpay'), $order['ID']); ?></p>
+            <p><?php printf(__('Pesanan untuk order INV %s telah dibatalkan. Terima kasih.', 'sejoli-xendit'), $order['ID']); ?></p>
         </div>
     </div>
 </div>
